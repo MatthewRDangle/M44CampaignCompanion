6e3ed2db8b0afa4b65b7be0d608b516d0c9a5731
matthewrdangle <matthewrdangle@gmail.com>
2020-11-09 17:14:53 -0500

    Set up a base game API.

--- Game/lib/js/game.js
+++ Game/lib/js/game.js
@@ -1,5 +1,10 @@
 "use strict"; // Execute JavaScript file in strict mode.
 
+//Configures the game options and loads it into the window.
+function gameLoader() {
+	let g = game.init(); // Initialize the Game.
+}
+
 /*
  * -----------------------------------------
  * ----------- Game API Library. -----------
@@ -8,9 +13,56 @@
  */
 
 // Protect the function scope.
-let gameAPI = {};
+let game = {};
 (function() {
 
+	/*
+	 * Property: Init.
+	 * Type: Method.
+	 * Description: Initizlizing the game.
+	 * 
+	 * @return Phaser.Game - Object - Always
+	 */
+	game.init = function() {
+		
+		// Create the game via Phaser.
+		let g = new Phaser.Game({
+			dom: { parent: 'Game' },
+			title: game.properties.name,
+			version: 'v' + game.properties.version.major  + '.' + game.properties.version.minor + '.' + game.properties.version.patch,
+			parent: game.properties.dom,
+			url: '',
+			type: Phaser.AUTO,
+			width: window.innerWidth * window.devicePixelRatio,
+			height: window.innerHeight * window.devicePixelRatio,
+			disableContextMenu: true,
+			scene: [WarSim]
+		});
+		
+		// Make game responsive.
+		window.addEventListener("resize", () => {
+		  let width = window.innerWidth * window.devicePixelRatio;
+		  let height = window.innerHeight * window.devicePixelRatio;
+		  g.scale.resize(width, height);
+		  g.scene.scenes.forEach(function (scene) {
+		    scene.cameras.main.setViewport(0, 0, width, height);
+		  });
+		},false);
+		
+		// Return game object.
+		return g;
+	}
 	
-	
+	/*
+	 * Function: Init.
+	 * Type: Properties.
+	 * Description: Storage for global game information.
+	 */
+	game.properties = {
+		name: 'Memoir 44: World War',
+		version: {major: '1', minor: '0', patch: '0'},
+		dom: document.getElementById('Game')
+	}
 }());
+
+gameLoader(); // Initiate the game.
---
+++ Game/lib/scenes/WarSim.js
@@ -0,0 +1,11 @@
+"use strict"; // Execute JavaScript file in strict mode.
+
+class WarSim extends Phaser.Scene {
+	
+    constructor() {
+        super('WarSim');
+    }
+    preload() {}
+    create() {}
+    update() {}
+}
