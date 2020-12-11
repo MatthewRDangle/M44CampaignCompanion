9048b517e8708751cd7212a1843ab095a4666d42
Matthew R Dangle <MatthewRDangle@gmail.com>
2020-12-11 18:59:06 -0500

    Support for all five factions.

--- public/lib/js/system/scenarios/scenario.js
+++ public/lib/js/system/scenarios/scenario.js
@@ -23,6 +23,12 @@ class Scenario {
 					this.factions.push( new Faction('America', 'USAFlag') );
 				else if (faction === 'Germany')
 					this.factions.push( new Faction('Germany', 'GermanFlag') );
+				else if (faction === 'UK')
+					this.factions.push( new Faction('UK', 'UKFlag') );
+				else if (faction === 'Soviets')
+					this.factions.push( new Faction('Soviets', 'SovietFlag') );
+				else if (faction === 'Japan')
+					this.factions.push( new Faction('Japan', 'JapanFlag') );
 			}
 		}
 	}
