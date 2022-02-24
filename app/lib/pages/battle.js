aef045cca597b81e67a0e49649dfa579ec98f82f
Matthew R Dangle <MatthewRDangle@gmail.com>
2022-02-23 19:40:49 -0500

    Battle Results Layout

--- app/lib/pages/battle.js
+++ app/lib/pages/battle.js
@@ -6,10 +6,44 @@ import {localData} from "../../localdata.js";
 export const page = new Page('/battle');
 page.setPage(function() {
     return [
-        m('div', {style: 'width: 100vw; height: 100vh;'},
-            m('div.preview',
-                m('img.preview_img', {src: 'lib/images/placeholder.PNG', alt: ''})
-            )
-        )
+        m('h1', 'Number of Survivors'),
+        m('div.results', [
+            m('h2.results_header', 'Attacker'),
+            m('div.results_counters', [
+                m('div.flex', [
+                    m('div.flex_item', [
+                        m('div.unitCard', [
+                            m('div.unitCard_name', 'Infantry')
+                        ]),
+                        m('input', {type: 'number', min: 0, max: 100, value: 0})
+                    ]),
+                    m('div.flex_item', [
+                        m('div.unitCard', [
+                            m('div.unitCard_name', 'Tanks')
+                        ]),
+                        m('input', {type: 'number', min: 0, max: 100, value: 0})
+                    ])
+                ])
+            ])
+        ]),
+        m('div.results', [
+            m('h2.results_header', 'Defender'),
+            m('div.results_counters', [
+                m('div.flex', [
+                    m('div.flex_item', [
+                        m('div.unitCard', [
+                            m('div.unitCard_name', 'Infantry')
+                        ]),
+                        m('input', {type: 'number', min: 0, max: 100, value: 0})
+                    ]),
+                    m('div.flex_item', [
+                        m('div.unitCard', [
+                            m('div.unitCard_name', 'Tanks')
+                        ]),
+                        m('input', {type: 'number', min: 0, max: 100, value: 0})
+                    ])
+                ])
+            ])
+        ])
     ]
 });
