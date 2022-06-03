e78c869b95dc1d266308e9f84e68d3b4365bc3e0
Matthew R Dangle <MatthewRDangle@gmail.com>
2022-06-02 20:37:51 -0400

    List available scenarios.

--- app/lib/pages/lobby.js
+++ app/lib/pages/lobby.js
@@ -9,24 +9,20 @@ export const page = new Page('/lobby', (initialVnode) => {
     const {loadScenarioFileRegistry, setActiveScenario} = scenarioStore;
     loadScenarioFileRegistry().then(() => m.redraw());
 
+    const handleOnClick = (registryItem) => {
+        setActiveScenario(registryItem.id)
+        m.route.set('/scenario?:id', {id: registryItem.id});
+    }
+
     return {
         view: (vNode) => {
             const {attrs} = vNode;
             const {fileRegistryList} = scenarioStore;
 
             return [
-                m(MainMenu, {currentPage: this, title: 'Campaign Companion'}, [
-                    m('div', {onclick: () => {
-                        setActiveScenario(fileRegistryList[0].id)
-                        m.route.set('/scenario?:id', {id: fileRegistryList[0].id});
-                    }}, 'Enter War Room >>'),
-                    m('div', {onclick: () => {m.route.set('/mainMenu')}}, '<< Back'),
-                ]),
-                m('div.campaignSelector',[
-                    m('div.campaignSelector_nav', fileRegistryList.map(() =>
-                        m('div.campaignSelector_nav_box')
-                    ))
-                ])
+                m(MainMenu, {currentPage: this, title: 'Campaign Companion'}, fileRegistryList.map(registryItem =>
+                    m('div', {onclick: () => {handleOnClick(registryItem)}}, registryItem.displayName)
+                ))
             ]
         }
     }
