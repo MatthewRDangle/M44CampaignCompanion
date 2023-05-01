3ee427bff326fdf24279de3e7bc3e52cafef8fb5
Matthew Dangle <matthewrdangle@gmail.com>
2023-04-30 21:04:08 -0400

    fix service ipc invokers

--- ipc/handles/system.js
+++ ipc/handles/system.js
@@ -3,7 +3,7 @@ const path = require ("path");
 const {ipcMain} = require("electron");
 
 const systemHandle = (channel) => {
-    ipcMain.handle(channel + '/getFileContent', (path) => {
+    ipcMain.handle(channel + '/getFileContent', (e, path) => {
         return new Promise(async resolve => {
             fs.readFile(path, 'utf8', (err, data) => {
                 resolve(data);
