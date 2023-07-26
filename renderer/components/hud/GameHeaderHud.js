77b5e9e4be1ef694b1c3d9cd7ec06c5b82297814
Matthew Dangle <matthewrdangle@gmail.com>
2023-07-25 21:22:51 -0400

    Add current turn count to hud

--- renderer/components/hud/GameHeaderHud.js
+++ renderer/components/hud/GameHeaderHud.js
@@ -45,6 +45,7 @@ const GameHeaderHud = (initialVnode) => {
                         ),
                         m('span', {className: 'inline-block ml-20 mt-1'}, activeScenarioDefinition.currentTurn.name)
                     ]),
+                    m('span', `Turn ${activeScenarioDefinition.turnCounter}`),
                     m('button', {onclick: handleOptionsOverlay}, [
                         m('div', {className: 'w-6 h-1 bg-interaction-700 mt-1 mb-1 rounded-full'}),
                         m('div', {className: 'w-6 h-1 bg-interaction-700 mt-1 mb-1 rounded-full'}),
